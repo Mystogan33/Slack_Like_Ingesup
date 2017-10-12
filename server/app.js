48c4bef538e373dfc5efaf1f1eea3de30d60d762
Valentin GAY <mystogan40@gmail.com>
2017-10-12 16:45:27 +0200

    Ajout d'une route Bonjour

--- server/app.js
+++ server/app.js
@@ -2,8 +2,12 @@ const express = require('express')
 const app = express()
 
 app.get('/', function (req, res) {
-  res.send('Server works !')
+  res.send('Server works !');
 })
+
+app.get('/:nom', function(req, res) {
+  res.send('Bonjour ' + req.params.nom);
+});
 
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!')
